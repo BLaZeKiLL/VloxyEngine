32a405c164871df41ffc8de00af15fd1bc2de61f
BLaZeKiLL <blazeingamer10@gmail.com>
2020-11-06 12:37:36 +0530

    basic profiling and graphy added

--- Assets/Scripts/Voxel/Colored/World/SimpleWorld.cs
+++ Assets/Scripts/Voxel/Colored/World/SimpleWorld.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 using CodeBlaze.Voxel.Colored.Block;
 using CodeBlaze.Voxel.Colored.Chunk;
@@ -7,6 +9,9 @@ using CodeBlaze.Voxel.Colored.Mesher;
 using CodeBlaze.Voxel.Engine.Core.Renderer;
 
 using UnityEngine;
+using UnityEngine.Profiling;
+
+using Debug = UnityEngine.Debug;
 
 namespace CodeBlaze.Voxel.Colored.World {
 
@@ -18,12 +23,18 @@ namespace CodeBlaze.Voxel.Colored.World {
         [SerializeField] private Material _material;
         [SerializeField] private bool _chunkShadows;
 
-        private Queue<ColoredChunk> _chunks;
+        //private Dictionary<Vector3Int, ColoredChunk> _chunks;
+        private Queue<ColoredChunk> _buildQueue;
         private ColoredGreedyMesher _mesher;
 
+        private Stopwatch _stopwatch;
+        
         private void Awake() {
-            _chunks = new Queue<ColoredChunk>();
+            //_chunks = new Dictionary<Vector3Int, ColoredChunk>();
+            _buildQueue = new Queue<ColoredChunk>();
             _mesher = new ColoredGreedyMesher();
+            
+            _stopwatch = new Stopwatch();
         }
 
         private void Start() {
@@ -32,20 +43,28 @@ namespace CodeBlaze.Voxel.Colored.World {
             
             for (int x = -_drawSize; x <= _drawSize; x++) {
                 for (int z = -_drawSize; z <= _drawSize; z++) {
-                    _chunks.Enqueue(CreateChunk(chunkSizeInt, new Vector3Int(chunkSizeInt.x * x, 0, chunkSizeInt.z * z), ++id));
+                    var chunk = CreateChunk(chunkSizeInt, new Vector3Int(chunkSizeInt.x * x, 0, chunkSizeInt.z * z),
+                        ++id);
+                    //_chunks.Add(chunk.Position, chunk);
+                    _buildQueue.Enqueue(chunk);
                 }
             }
+
+            StartCoroutine(SpawnChunks());
         }
 
-        private void Update() {
-            if (_chunks.Count <= 0) return;
+        private IEnumerator SpawnChunks() {
+            while (_buildQueue.Count > 0) {
+                SpawnChunk();
 
-            // convert to courotine
-            SpawnChunk();
+                yield return null;
+            }
+            
+            GC.Collect();
         }
 
         private void SpawnChunk() {
-            var chunk = _chunks.Dequeue();
+            var chunk = _buildQueue.Dequeue();
 
             var go = new GameObject($"Chunk-{chunk.ID}", typeof(ChunkRenderer));
             go.transform.parent = transform;
@@ -53,8 +72,16 @@ namespace CodeBlaze.Voxel.Colored.World {
             
             var chunkRenderer = go.GetComponent<ChunkRenderer>();
             chunkRenderer.SetRenderSettings(_material, _chunkShadows);
-            chunkRenderer.Render(_mesher.GenerateMesh(chunk));
             
+            _stopwatch.Start();
+            var data = _mesher.GenerateMesh(chunk);
+            _stopwatch.Stop();
+            
+            Debug.Log($"CHUNK-{chunk.ID} MESH BUILD TIME : {_stopwatch.ElapsedMilliseconds}");
+            
+            _stopwatch.Reset();
+            
+            chunkRenderer.Render(data);
             _mesher.Clear();
         }
         
