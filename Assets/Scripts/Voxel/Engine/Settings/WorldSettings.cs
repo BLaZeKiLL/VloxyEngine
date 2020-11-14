3add9330ceda2bb2ff43bd864095b128d52b65ed
BLaZeKiLL <blazeingamer10@gmail.com>
2020-11-14 18:56:33 +0530

    fixis infinite generation issues

--- Assets/Scripts/Voxel/Engine/Settings/WorldSettings.cs
+++ Assets/Scripts/Voxel/Engine/Settings/WorldSettings.cs
@@ -8,7 +8,8 @@ namespace CodeBlaze.Voxel.Engine.Settings {
     public class WorldSettings {
 
         public Transform Focus;
-        public int DrawSize = 1;
+        public int ChunkPageSize = 25;
+        public int DrawDistance = 1;
         [Range(0.01f, 0.99f)] public float Frequency = 0.15f;
         public Vector3Int ChunkSize = 16 * Vector3Int.one; // this is serialized, rider issue
 
