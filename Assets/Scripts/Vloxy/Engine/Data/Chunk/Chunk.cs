a55ee9df87dd92a8958f60569d067ebf185d2b47
BLaZeKiLL <blazeingamer10@gmail.com>
2021-12-09 00:58:16 +0530

    Chunk State removed

--- Assets/Scripts/Vloxy/Engine/Data/Chunk/Chunk.cs
+++ Assets/Scripts/Vloxy/Engine/Data/Chunk/Chunk.cs
@@ -4,17 +4,13 @@ namespace CodeBlaze.Vloxy.Engine.Data {
 
     public struct Chunk {
 
-        // TODO : initialize chunk data
-        public NativeChunkData Data { get; set; }
+        public NativeChunkData Data { get; }
         
         public int3 Position { get; }
-        
-        internal ChunkState State { get; set; }
         
         public Chunk(int3 position, NativeChunkData data) {
             Position = position;
             Data = data;
-            State = ChunkState.INACTIVE;
         }
 
     }
