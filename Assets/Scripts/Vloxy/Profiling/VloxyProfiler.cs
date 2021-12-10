8416f70d284c68541e6ba6b0817a3e7bf01809a3
Devashish Lal <blazeingamer10@gmail.com>
2021-12-10 16:58:37 +0530

    Debug Tools Added (#40)
    * Height Map Gizmo and Profiler Markers added
    
    * stash

---
+++ Assets/Scripts/Vloxy/Profiling/VloxyProfiler.cs
@@ -0,0 +1,24 @@
+﻿using Unity.Profiling;
+
+namespace CodeBlaze.Vloxy.Profiling {
+
+    public static class VloxyProfiler {
+
+        public static ProfilerMarker MeshBuildJobMarker = new("MeshBuildJob");
+        public static ProfilerMarker ViewRegionUpdateMarker = new("ViewRegionUpdate");
+
+        public static ProfilerRecorder MeshBuildJobRecorder;
+        
+        public static void Initialize() {
+            MeshBuildJobRecorder = ProfilerRecorder.StartNew(MeshBuildJobMarker);
+        }
+
+        public static void Dispose() {
+            MeshBuildJobRecorder.Dispose();
+        }
+
+        public static string TimeMS(this ProfilerRecorder recorder) => $"{recorder.CurrentValueAsDouble * (1e-6f):F}ms";
+
+    }
+
+}
