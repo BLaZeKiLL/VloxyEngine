bfb2fa7af9dcd97166389d6f0b31da8b21fffac9
Devashish Lal <blazeingamer10@gmail.com>
2021-12-19 16:57:00 +0530

    builders added (#62)

---
+++ Assets/Editor/Build/Builder.cs
@@ -0,0 +1,90 @@
+﻿using UnityEditor;
+using UnityEditor.Build.Reporting;
+
+namespace CodeBlaze.Editor.Build {
+
+    public static class Builder {
+
+        [MenuItem("Build/Mono/Debug")]
+        private static void BuildMonoDebug() {
+            PlayerSettings.SetScriptingBackend(BuildTargetGroup.Standalone, ScriptingImplementation.Mono2x);
+            
+            var options = new BuildPlayerOptions {
+                scenes = EditorBuildSettingsScene.GetActiveSceneList(EditorBuildSettings.scenes),
+                locationPathName = "Build/Mono-Debug/vloxy_mono_debug.exe",
+                target = BuildTarget.StandaloneWindows64,
+                targetGroup = BuildTargetGroup.Standalone,
+                extraScriptingDefines = new [] { "UNITY_POST_PROCESSING_STACK_V2", "VLOXY_LOGGING", "VLOXY_DEBUG", "VLOXY_PROFILING" },
+                options = BuildOptions.Development | BuildOptions.AllowDebugging | BuildOptions.CompressWithLz4HC
+            };
+
+            var report = BuildPipeline.BuildPlayer(options);
+
+            if (report.summary.result == BuildResult.Succeeded) {
+                EditorUtility.RevealInFinder("Build/Mono-Debug");
+            }
+        }
+
+        [MenuItem("Build/Mono/Release")]
+        private static void BuildMonoRelease() {
+            PlayerSettings.SetScriptingBackend(BuildTargetGroup.Standalone, ScriptingImplementation.Mono2x);
+            
+            var options = new BuildPlayerOptions {
+                scenes = EditorBuildSettingsScene.GetActiveSceneList(EditorBuildSettings.scenes),
+                locationPathName = "Build/Mono-Release/vloxy_mono_release.exe",
+                target = BuildTarget.StandaloneWindows64,
+                targetGroup = BuildTargetGroup.Standalone,
+                extraScriptingDefines = new [] { "UNITY_POST_PROCESSING_STACK_V2" },
+                options = BuildOptions.CompressWithLz4HC
+            };
+
+            var report = BuildPipeline.BuildPlayer(options);
+
+            if (report.summary.result == BuildResult.Succeeded) {
+                EditorUtility.RevealInFinder("Build/Mono-Release");
+            }
+        }
+
+        [MenuItem("Build/IL2CPP/Debug")]
+        private static void BuildIL2CPPDebug() {
+            PlayerSettings.SetScriptingBackend(BuildTargetGroup.Standalone, ScriptingImplementation.IL2CPP);
+            
+            var options = new BuildPlayerOptions {
+                scenes = EditorBuildSettingsScene.GetActiveSceneList(EditorBuildSettings.scenes),
+                locationPathName = "Build/IL2CPP-Debug/vloxy_il2cpp_debug.exe",
+                target = BuildTarget.StandaloneWindows64,
+                targetGroup = BuildTargetGroup.Standalone,
+                extraScriptingDefines = new [] { "UNITY_POST_PROCESSING_STACK_V2", "VLOXY_LOGGING", "VLOXY_DEBUG", "VLOXY_PROFILING" },
+                options = BuildOptions.Development | BuildOptions.AllowDebugging | BuildOptions.CompressWithLz4HC
+            };
+
+            var report = BuildPipeline.BuildPlayer(options);
+
+            if (report.summary.result == BuildResult.Succeeded) {
+                EditorUtility.RevealInFinder("Build/IL2CPP-Debug");
+            }
+        }
+
+        [MenuItem("Build/IL2CPP/Release")]
+        private static void BuildIL2CPPRelease() {
+            PlayerSettings.SetScriptingBackend(BuildTargetGroup.Standalone, ScriptingImplementation.IL2CPP);
+            
+            var options = new BuildPlayerOptions {
+                scenes = EditorBuildSettingsScene.GetActiveSceneList(EditorBuildSettings.scenes),
+                locationPathName = "Build/IL2CPP-Release/vloxy_il2cpp_release.exe",
+                target = BuildTarget.StandaloneWindows64,
+                targetGroup = BuildTargetGroup.Standalone,
+                extraScriptingDefines = new [] { "UNITY_POST_PROCESSING_STACK_V2" },
+                options = BuildOptions.CompressWithLz4HC
+            };
+
+            var report = BuildPipeline.BuildPlayer(options);
+
+            if (report.summary.result == BuildResult.Succeeded) {
+                EditorUtility.RevealInFinder("Build/IL2CPP-Release");
+            }
+        }
+
+    }
+
+}
