19bcbfcc201c6028d4af00bc5c40587b37b20ab5
Devashish Lal <laldevashish@gmail.com>
2023-03-06 19:26:15 -0800

    priority queue added

---
+++ Packages/io.codeblaze.vloxyengine/Runtime/Engine/ThirdParty/Priority Queue/FastPriorityQueueNode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Priority_Queue
+{
+    public class FastPriorityQueueNode
+    {
+        /// <summary>
+        /// The Priority to insert this node at.
+        /// Cannot be manually edited - see queue.Enqueue() and queue.UpdatePriority() instead
+        /// </summary>
+        public float Priority { get; protected internal set; }
+
+        /// <summary>
+        /// Represents the current position in the queue
+        /// </summary>
+        public int QueueIndex { get; internal set; }
+
+#if DEBUG
+        /// <summary>
+        /// The queue this node is tied to. Used only for debug builds.
+        /// </summary>
+        public object Queue { get; internal set; }
+#endif
+    }
+}
