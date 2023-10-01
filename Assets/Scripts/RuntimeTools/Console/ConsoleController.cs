dc83b8470292e6030edf69aace5613b4844df268
Devashish Lal <laldevashish@gmail.com>
2023-10-01 16:20:13 -0700

    Console improcements

--- Assets/Scripts/RuntimeTools/Console/ConsoleController.cs
+++ Assets/Scripts/RuntimeTools/Console/ConsoleController.cs
@@ -1,12 +1,40 @@
+using System;
+using CodeBlaze.Vloxy.Demo.Managers;
+using CodeBlaze.Vloxy.Demo.Utils;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace CodeBlaze.Vloxy.Demo.RuntimeTools.Console {
 
     public class ConsoleController : MonoBehaviour {
 
-        [SerializeField] private InputField Input;
+        [SerializeField] private TMP_InputField CommandLine;
         [SerializeField] private ScrollRect Output;
+
+        private void OnEnable() {
+            GameManager.Current.InputMaps.Player.Disable();
+            
+            CommandLine.ActivateInputField();
+            CommandLine.onSubmit.AddListener(OnCommand);
+            CommandLine.Select();
+        }
+
+        private void OnDisable() {
+            GameManager.Current.InputMaps.Player.Enable();
+            
+            CommandLine.ReleaseSelection();
+            CommandLine.onSubmit.RemoveAllListeners();
+            CommandLine.DeactivateInputField();
+        }
+        
+        private void OnCommand(string command) {
+            CommandLine.text = string.Empty;
+            
+            GameLogger.Info<ConsoleController>(command);
+        }
 
     }
 
