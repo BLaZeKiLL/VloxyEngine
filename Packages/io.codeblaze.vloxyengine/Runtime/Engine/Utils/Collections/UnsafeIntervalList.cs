bd6a9d94b592c9d434ec9ecdaaf44e04f0d4fb83
Devashish Lal <laldevashish@gmail.com>
2023-10-01 01:42:17 -0700

    Setter tests

--- Packages/io.codeblaze.vloxyengine/Runtime/Engine/Utils/Collections/UnsafeIntervalList.cs
+++ Packages/io.codeblaze.vloxyengine/Runtime/Engine/Utils/Collections/UnsafeIntervalList.cs
@@ -11,7 +11,7 @@ namespace CodeBlaze.Vloxy.Engine.Utils.Collections {
     /// Didn't see much difference in SOA or AOS performance wise, performance depends on the way elements
     /// would be accessed in ou case AOS might be better, should profile and see
     /// </summary>
-    [BurstCompile]
+    // [BurstCompile]
     public struct UnsafeIntervalList {
 
         // Array of structs impl
@@ -70,12 +70,20 @@ namespace CodeBlaze.Vloxy.Engine.Utils.Collections {
         public void Dispose() {
             Internal.Dispose();
         }
+        
+        public int NodeIndex(int index) => BinarySearch(index);
 
         public void AddInterval(int id, int count) {
             Length += count;
             Internal.Add(new Node(id, Length));
         }
 
+        /// <summary>
+        /// COMPLEXITY : O(Log(n))
+        /// </summary>
+        /// <param name="index">Index at which value to fetch</param>
+        /// <returns>Value at index</returns>
+        /// <exception cref="IndexOutOfRangeException">Debug only</exception>
         public int Get(int index) {
 #if UNITY_EDITOR || DEVELOPMENT_BUILD
             if (index >= Length) throw new IndexOutOfRangeException($"{index} is out of range for the given data of length {Length}");
@@ -83,17 +91,133 @@ namespace CodeBlaze.Vloxy.Engine.Utils.Collections {
             return Internal[BinarySearch(index)].ID;
         }
 
+        /// <summary>
+        /// Not sure if I got all the cases
+        /// COMPLEXITY : O(Log(n)), could be O(n) also depending on RemoveRange and InsertRange
+        /// REF: https://github.com/mikolalysenko/NodeMinecraftThing/blob/master/client/voxels.js
+        /// </summary>
+        /// <param name="index">index to set at</param>
+        /// <param name="id">value to set at</param>
+        /// <exception cref="IndexOutOfRangeException">Debug only</exception>
         public void Set(int index, int id) {
-            // TODO : To Implement (REF: https://github.com/mikolalysenko/NodeMinecraftThing/blob/master/client/voxels.js)
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
+            if (index >= Length) throw new IndexOutOfRangeException($"{index} is out of range for the given data of length {Length}");
+#endif
+            
+            var node_index = BinarySearch(index);
+
+            var block = Internal[node_index].ID;
+            
+            if (block == id) return; // No Change
+            
+            var (left_item, left_node_index) = LeftOf(index, node_index);
+            var (right_item, right_node_index) = RightOf(index, node_index);
+            
+            if (id == left_item && id == right_item) { // [X,A,X] -> [X,X,X]
+                var left_node = Internal[left_node_index]; // This is returned by valued
+
+                left_node.Count = Internal[right_node_index].Count;
+
+                Internal[left_node_index] = left_node; // Need to update it
+                
+                Internal.RemoveRange(node_index, 2); // Can remove node and right
+            } else if (id == left_item) { // [X,A,Y] -> [X,X,Y]
+                var left_node = Internal[left_node_index];
+
+                left_node.Count++;
+
+                Internal[left_node_index] = left_node;
+                
+                Internal.RemoveRange(node_index, 1);
+            } else if (id == right_item) { // [X,A,Y] -> [X,Y,Y]
+                Internal.RemoveRange(node_index, 1);
+            } else { // No Coalesce
+                if (block == left_item && block == right_item) { // [X,X,X] -> [X,A,X]
+                    Internal.InsertRange(node_index, 2);
+
+                    var left_node = Internal[node_index];
+                    var node = Internal[node_index + 1];
+                    var right_node = Internal[node_index + 2];
+
+                    left_node.Count = index;
+
+                    node.ID = id;
+                    node.Count = index + 1;
+
+                    right_node.ID = left_node.ID;
+
+                    Internal[node_index] = left_node;
+                    Internal[node_index + 1] = node;
+                    Internal[node_index + 2] = right_node;
+                } else if (block != left_item && block == right_item) { // [X,Y,Y] -> [X,A,Y]
+                    Internal.InsertRange(node_index, 1);
+
+                    var node = Internal[node_index];
+
+                    node.ID = id;
+                    node.Count = Internal[left_node_index].Count + 1;
+
+                    Internal[node_index] = node;
+                } else if (block == left_item && block != right_item) { // [X,X,Y] -> [X,A,Y]
+                    Internal.InsertRange(node_index, 1);
+
+                    var node = Internal[node_index + 1];
+                    var left_node = Internal[left_node_index];
+                    
+                    node.ID = id;
+                    node.Count = left_node.Count;
+
+                    left_node.Count--;
+
+                    Internal[node_index + 1] = node;
+                    Internal[left_node_index] = left_node;
+                } else { // [X,Y,X] -> [X,A,X] or [X,Y,Z] -> [X,A,Z]
+                    var node = Internal[node_index];
+
+                    node.ID = id;
+
+                    Internal[node_index] = node;
+                }
+            }
+        }
+
+        public int LeftOf(int index) {
+            return LeftOf(index, NodeIndex(index)).Item1;
+        }
+
+        public int RightOf(int index) {
+            return RightOf(index, NodeIndex(index)).Item1;
+        }
+        
+        private (int, int) LeftOf(int index, int node_index) {
+            if (node_index == 0) { // First Node
+                return index == 0 ? (-1, -1) : (Internal[node_index].ID, node_index);
+            }
+
+            var left = Internal[node_index - 1];
+            var node = Internal[node_index];
+
+            return index - 1 < left.Count ? (left.ID, node_index - 1) : (node.ID, node_index);
+        }
+
+        private (int, int) RightOf(int index, int node_index) {
+            if (node_index == CompressedLength - 1) { // Last Node
+                return index == Length - 1 ? (-1, -1) : (Internal[node_index].ID, node_index);
+            } 
+            
+            var right = Internal[node_index + 1];
+            var node = Internal[node_index];
+
+            return index + 1 >= node.Count ? (right.ID, node_index + 1) : (node.ID, node_index);
         }
 
         private int BinarySearch(int index) {
-            int min = 0;
-            int max = Internal.Length;
+            var min = 0;
+            var max = Internal.Length;
 
             while (min <= max) {
-                int mid = (max + min) / 2;
-                int count = Internal[mid].Count;
+                var mid = (max + min) / 2;
+                var count = Internal[mid].Count;
 
                 if (index == count) return mid + 1;
                 
--- Packages/io.codeblaze.vloxyengine/Tests/Editor/Utils/Collections/UnsafeIntervalListTest.cs
+++ Packages/io.codeblaze.vloxyengine/Tests/Editor/Utils/Collections/UnsafeIntervalListTest.cs
@@ -15,8 +15,8 @@ namespace CodeBlaze.Vloxy.Engine.Tests.Editor.Utils.Collections {
             
             var compressed = new UnsafeIntervalList(list, 16, Allocator.Temp);
 
-            Assert.AreEqual(compressed.Length, list.Length);
-            Assert.AreEqual(compressed.CompressedLength, 1);
+            Assert.AreEqual(list.Length, compressed.Length);
+            Assert.AreEqual(1, compressed.CompressedLength);
 
             list.Dispose();
             compressed.Dispose();
@@ -34,28 +34,251 @@ namespace CodeBlaze.Vloxy.Engine.Tests.Editor.Utils.Collections {
             
             Assert.AreEqual(compressed.CompressedLength, 3);
             
-            Assert.AreEqual(compressed.Get(Random.Range(0, 5)), 1);
-            Assert.AreEqual(compressed.Get(Random.Range(5, 10)), 2);
-            Assert.AreEqual(compressed.Get(Random.Range(10, 15)), 3);
+            Assert.AreEqual(1, compressed.Get(Random.Range(0, 5)));
+            Assert.AreEqual(2, compressed.Get(Random.Range(5, 10)));
+            Assert.AreEqual(3, compressed.Get(Random.Range(10, 15)));
 
+            list.Dispose();
+            compressed.Dispose();
+        }
+
+        [Test]
+        public void ShouldReturnLeftRightInSameNode() {
+            var list = new NativeList<int>(16, Allocator.Temp);
+            
+            list.AddReplicate(1, 5);
+
+            var compressed = new UnsafeIntervalList(list, 16, Allocator.Temp);
+
+            Assert.AreEqual(1, compressed.LeftOf(2));
+            Assert.AreEqual(1, compressed.RightOf(2));
+            
+            list.Dispose();
+            compressed.Dispose();
+        }
+
+        [Test]
+        public void ShouldReturnLeftOfOnLeftBoundary() {
+            var list = new NativeList<int>(16, Allocator.Temp);
+            
+            list.AddReplicate(1, 5);
+            list.AddReplicate(2, 5);
+
+            var compressed = new UnsafeIntervalList(list, 16, Allocator.Temp);
+
+            Assert.AreEqual(1, compressed.LeftOf(5));
+            
+            list.Dispose();
+            compressed.Dispose();
+        }
+
+        [Test]
+        public void ShouldReturnRightOfOnRightBoundary() {
+            var list = new NativeList<int>(16, Allocator.Temp);
+            
+            list.AddReplicate(1, 5);
+            list.AddReplicate(2, 5);
+
+            var compressed = new UnsafeIntervalList(list, 16, Allocator.Temp);
+            
+            Assert.AreEqual(2, compressed.RightOf(4));
+            
+            list.Dispose();
+            compressed.Dispose();
+        }
+
+        [Test]
+        public void ShouldReturnLeftRightOfSingeLengthNode() {
+            var list = new NativeList<int>(16, Allocator.Temp);
+            
+            list.AddReplicate(1, 5);
+            list.AddReplicate(2, 1);
+            list.AddReplicate(3, 5);
+
+            var compressed = new UnsafeIntervalList(list, 16, Allocator.Temp);
+
+            Assert.AreEqual(1, compressed.LeftOf(5));
+            Assert.AreEqual(3, compressed.RightOf(5));
+            
+            list.Dispose();
+            compressed.Dispose();
+        }
+
+        [Test]
+        public void ShouldSetRandomInterval() {
+            var list = new NativeList<int>(16, Allocator.Temp);
+
+            var a = Random.Range(1, 6);
+            var b = Random.Range(1, 6);
+            var c = Random.Range(1, 6);
+            
+            list.AddReplicate(1, a);
+            list.AddReplicate(2, b);
+            list.AddReplicate(3, c);
+
+            var compressed = new UnsafeIntervalList(list, 16, Allocator.Temp);
+
+            var index = Random.Range(1, a + b + c);
+            
+            compressed.Set(index, 4);
+            
+            Assert.AreEqual(4, compressed.Get(index));
+            
+            list.Dispose();
+            compressed.Dispose();
+        }
+
+        [Test]
+        public void ShouldSet_NoCoalesce_XYZ() {
+            var list = new NativeList<int>(16, Allocator.Temp);
+            
+            list.AddReplicate(1, 5);
+            list.AddReplicate(2, 1);
+            list.AddReplicate(3, 5);
+
+            var compressed = new UnsafeIntervalList(list, 16, Allocator.Temp);
+            
+            compressed.Set(5, 4);
+            
+            Assert.AreEqual(4, compressed.Get(5));
+            Assert.AreEqual(3, compressed.CompressedLength);
+            
             list.Dispose();
             compressed.Dispose();
         }
         
         [Test]
-        public void ShouldSetRandomInterval() { }
+        public void ShouldSet_NoCoalesce_XYX() {
+            var list = new NativeList<int>(16, Allocator.Temp);
+            
+            list.AddReplicate(1, 5);
+            list.AddReplicate(2, 1);
+            list.AddReplicate(3, 5);
+
+            var compressed = new UnsafeIntervalList(list, 16, Allocator.Temp);
+            
+            compressed.Set(5, 4);
+            
+            Assert.AreEqual(4, compressed.Get(5));
+            Assert.AreEqual(3, compressed.CompressedLength);
+            
+            list.Dispose();
+            compressed.Dispose();
+        }
         
         [Test]
-        public void ShouldSetInterval_NoCoalesce() { }
+        public void ShouldSet_NoCoalesce_XXX() {
+            var list = new NativeList<int>(16, Allocator.Temp);
+            
+            list.AddReplicate(1, 5);
+            list.AddReplicate(1, 1);
+            list.AddReplicate(1, 5);
+
+            var compressed = new UnsafeIntervalList(list, 16, Allocator.Temp);
+            
+            compressed.Set(5, 4);
+            
+            Assert.AreEqual(4, compressed.Get(5));
+            Assert.AreEqual(3, compressed.CompressedLength);
+            
+            list.Dispose();
+            compressed.Dispose();
+        }
         
         [Test]
-        public void ShouldSetInterval_LeftCoalesce() { }
+        public void ShouldSet_NoCoalesce_XXY() {
+            var list = new NativeList<int>(16, Allocator.Temp);
+            
+            list.AddReplicate(1, 5);
+            list.AddReplicate(1, 1);
+            list.AddReplicate(2, 5);
+
+            var compressed = new UnsafeIntervalList(list, 16, Allocator.Temp);
+            
+            compressed.Set(5, 4);
+            
+            Assert.AreEqual(4, compressed.Get(5));
+            Assert.AreEqual(3, compressed.CompressedLength);
+            
+            list.Dispose();
+            compressed.Dispose();
+        }
         
         [Test]
-        public void ShouldSetInterval_RightCoalesce() { }
-        
+        public void ShouldSet_NoCoalesce_XYY() {
+            var list = new NativeList<int>(16, Allocator.Temp);
+            
+            list.AddReplicate(1, 5);
+            list.AddReplicate(2, 1);
+            list.AddReplicate(2, 5);
+
+            var compressed = new UnsafeIntervalList(list, 16, Allocator.Temp);
+            
+            compressed.Set(5, 4);
+            
+            Assert.AreEqual(4, compressed.Get(5));
+            Assert.AreEqual(3, compressed.CompressedLength);
+            
+            list.Dispose();
+            compressed.Dispose();
+        }
+
         [Test]
-        public void ShouldSetInterval_BothCoalesce() { }
+        public void ShouldSet_LeftCoalesce() {
+            var list = new NativeList<int>(16, Allocator.Temp);
+            
+            list.AddReplicate(1, 5);
+            list.AddReplicate(2, 1);
+            list.AddReplicate(3, 5);
+
+            var compressed = new UnsafeIntervalList(list, 16, Allocator.Temp);
+            
+            compressed.Set(5, 1);
+            
+            Assert.AreEqual(1, compressed.Get(5));
+            Assert.AreEqual(2, compressed.CompressedLength);
+            
+            list.Dispose();
+            compressed.Dispose();
+        }
+
+        [Test]
+        public void ShouldSet_RightCoalesce() {
+            var list = new NativeList<int>(16, Allocator.Temp);
+            
+            list.AddReplicate(1, 5);
+            list.AddReplicate(2, 1);
+            list.AddReplicate(3, 5);
+
+            var compressed = new UnsafeIntervalList(list, 16, Allocator.Temp);
+            
+            compressed.Set(5, 3);
+            
+            Assert.AreEqual(3, compressed.Get(5));
+            Assert.AreEqual(2, compressed.CompressedLength);
+            
+            list.Dispose();
+            compressed.Dispose();
+        }
+
+        [Test]
+        public void ShouldSet_BothCoalesce() {
+            var list = new NativeList<int>(16, Allocator.Temp);
+            
+            list.AddReplicate(1, 5);
+            list.AddReplicate(2, 1);
+            list.AddReplicate(1, 5);
+
+            var compressed = new UnsafeIntervalList(list, 16, Allocator.Temp);
+
+            compressed.Set(5, 1);
+            
+            Assert.AreEqual(1, compressed.Get(5));
+            Assert.AreEqual(1, compressed.CompressedLength);
+            
+            list.Dispose();
+            compressed.Dispose();
+        }
     }
 
 }
